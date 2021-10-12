fe087fcc151825b5f69ce008e993b5028a248881
Luke Mariani <lukemariani1@gmail.com>
2021-10-12 15:06:50 +1100

    Fixed channel number

--- segmentation_models_pytorch_single/base/heads.py
+++ segmentation_models_pytorch_single/base/heads.py
@@ -13,7 +13,7 @@ from .modules import Activation
 class SegmentationHead(nn.Sequential):
 
     def __init__(self, in_channels, out_channels, kernel_size=3, activation=None, upsampling=1):
-        conv2d = nn.Conv2d(in_channels, 1, kernel_size=kernel_size, padding=kernel_size // 2)
+        conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
         #upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
         #activation = Activation(activation)
         super().__init__(conv2d)
